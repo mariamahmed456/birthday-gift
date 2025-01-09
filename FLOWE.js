7fea3efbebb224ef44f5b47137e033178e1e63ea
mariamahmed456 <139582691+mariamahmed456@users.noreply.github.com>
2025-01-10 01:46:07 +0200

    Add files via upload

---
+++ FLOWE.js
@@ -0,0 +1,24 @@
+let currentNumber = 1;
+const numberElement = document.getElementById('no');
+const actionButton = document.getElementById('action-button');
+numberElement.addEventListener('click', () => {
+    if (currentNumber < 19) {
+        numberElement.style.opacity = 0;
+
+    
+        setTimeout(() => {
+            currentNumber++;
+            numberElement.textContent = currentNumber;
+            
+            numberElement.style.opacity = 1;
+        }, 500); 
+    } else {
+    
+        actionButton.style.display = 'block';
+    }
+});
+
+
+actionButton.addEventListener('click', () => {
+    window.location.href = 'floo.html'; 
+});
