8bfdc6ee6ae54e89e1e4fa3f45582fbcb4a02df3
mariamahmed456 <139582691+mariamahmed456@users.noreply.github.com>
2025-01-14 18:47:45 +0200

    Update fff.js

--- fff.js
+++ fff.js
@@ -1,4 +1,10 @@
-
-onload = () => {
+window.onload = function() {
+  // Remove the 'container' class from the body element
   document.body.classList.remove("container");
+
+  // Play the audio and unmute it
+  var audio = document.querySelector('audio');
+  audio.play();
+  audio.muted = false; // Unmute after play starts
 };
+
