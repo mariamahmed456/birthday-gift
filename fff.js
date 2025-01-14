a35351fb732ecbb17441d3b9a6be091e14a7b9cf
mariamahmed456 <139582691+mariamahmed456@users.noreply.github.com>
2025-01-14 17:34:46 +0200

    Update fff.js

--- fff.js
+++ fff.js
@@ -1,3 +1,6 @@
 onload = () => {
-    document.body.classList.remove("container");
-  };
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
